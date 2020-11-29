1393b2966bd23d3aa7e89413f91584465af738ce
unknown <michal.nowak061@gmail.com>
2020-11-29 02:50:02 +0100

    insert_new_klient procedure added to db_init

--- SQL_scripts/db_init.sql
+++ SQL_scripts/db_init.sql
@@ -14,7 +14,7 @@ BEGIN
         ID_Adresu       INT             not null,
         Imie            VARCHAR(30)     not null,
         Nazwisko        VARCHAR(30)     not null,
-        DrugieImie      VARCHAR(30)     not null,
+        DrugieImie      VARCHAR(30)     null,
         PESEL           VARCHAR(11)     not null,
         NumerTel        VARCHAR(9)      not null,
         KatPrawaJazdy   CHAR(1)         not null,
@@ -190,10 +190,60 @@ CREATE INDEX idx_Wypozyczalnia_ID_Adresu    ON Wypozyczalnie(ID_Adresu);
 --Creating indexes to addresses table
 CREATE INDEX idx_Adres_KodPocztowy      ON Adresy(KodPocztowy);
 CREATE INDEX idx_Adres_Miejscowosc      ON Adresy(Miejscowosc);
+--PROCEDURES CONFIGURATION-----------------------------------------------------
+/
+create or replace procedure insert_new_adres(KodPocztowy in adresy.kodpocztowy%TYPE,
+                                             Miejscowosc in adresy.miejscowosc%TYPE,
+                                             Ulica in adresy.ulica%TYPE,
+                                             NumerDomu in adresy.numerdomu%TYPE,
+                                             NumerMieszkania in adresy.numermieszkania%TYPE) as 
+begin
+    DECLARE cnt INT;
+    BEGIN
+        SELECT COUNT(*) INTO cnt FROM adresy WHERE NOT EXISTS(SELECT *
+                                                              FROM adresy
+                                                              WHERE kodpocztowy = KodPocztowy and
+                                                                    miejscowosc = Miejscowosc);
+        IF(cnt = 0) THEN
+            INSERT INTO Adresy(kodpocztowy, miejscowosc, ulica, numerdomu, numermieszkania)
+            VALUES (KodPocztowy, Miejscowosc, Ulica, NumerDomu, NumerMieszkania);
+        END IF;
+    END;
+end insert_new_adres;
+/
+create or replace procedure insert_new_klient(Imie in klienci.imie%TYPE,
+                                              DrugieImie in klienci.drugieimie%TYPE default NULL,
+                                              Nazwisko in klienci.nazwisko%TYPE,
+                                              PESEL in klienci.pesel%TYPE,
+                                              NrTel in klienci.numertel%TYPE,
+                                              KategoriaPrawaJazdy in klienci.katprawajazdy%TYPE,
+                                              KodPocztowy in adresy.kodpocztowy%TYPE,
+                                              Miejscowosc in adresy.miejscowosc%TYPE,
+                                              Ulica in adresy.ulica%TYPE,
+                                              NumerDomu in adresy.numerdomu%TYPE,
+                                              NumerMieszkania in adresy.numermieszkania%TYPE default NULL) as 
+begin
+    insert_new_adres(KodPocztowy, Miejscowosc, Ulica, NumerDomu, NumerMieszkania);
+    declare index_value INT;
+    BEGIN
+        select ID_Adresu into index_value from Adresy where kodpocztowy = KodPocztowy and
+                                                          miejscowosc = Miejscowosc and 
+                                                          ulica = Ulica and
+                                                          numerdomu = NumerDomu and
+                                                          numermieszkania = NumerMieszkania;
+
+        INSERT INTO Klienci(id_adresu, imie, drugieimie, nazwisko, pesel, numertel, katprawajazdy)
+        VALUES (index_value, Imie, DrugieImie, Nazwisko, PESEL, NrTel, KategoriaPrawaJazdy);
+    END;
+end insert_new_klient;
+/
 --DEFAULT DATA CONFIGURATION---------------------------------------------------
 BEGIN
-   insert_new_adress('59-700', 'Boleslawiec', 'Sierpnia', 80);
+    insert_new_adres('53-225', 'Buczacz', 'Narcyzowa', 11, 2);
+    insert_new_adres('53-225', 'Buczacz', 'Narcyzowa', 11, 1);
+    insert_new_adres('59-700', 'Boleslawiec', 'Jarzebinowa', 7, 4);
+   --insert_new_klient('Michal', 'Stanislaw', 'Nowak', '97070206830', '123456789', 'B', '53-225', 'Buczacz', 'Narcyzowa', 11, 2);
+   --insert_new_klient('Adam', NULL, 'Krzykala', '12345678919', '123456789', 'B', '59-700', 'Boleslawiec', 'Jarzebinowa', 7, 4);
 END;
 -------------------------------------------------------------------------------
-
 commit;
