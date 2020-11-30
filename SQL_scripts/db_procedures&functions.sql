893c895d89958ee0da029e38c00c2fc8add4d3e0
unknown <michal.nowak061@gmail.com>
2020-11-30 15:05:52 +0100

    insert_new_model and insert_new_pojazd added

--- SQL_scripts/db_procedures&functions.sql
+++ SQL_scripts/db_procedures&functions.sql
@@ -49,5 +49,57 @@ BEGIN
     END;
 END;
 /
+create or replace procedure insert_new_model(input_Model in modele.model%TYPE,
+                                             input_Pojemnosc in modele.pojemnoscsilnika%TYPE,
+                                             input_Spalanie in modele.sredniespalanie%TYPE,
+                                             input_KatPrawaJazdy in modele.kategoriaprawajazdy%TYPE,
+                                             input_Stawka in modele.stawkazadzien%TYPE) IS 
+BEGIN
+    DECLARE 
+    cnt INT := 0;
+    BEGIN
+        SELECT COUNT(*) INTO cnt FROM Modele WHERE input_Model in modele.model AND
+                                                   input_Pojemnosc in modele.pojemnoscsilnika AND
+                                                   input_Spalanie in modele.sredniespalanie AND
+                                                   input_KatPrawaJazdy in modele.kategoriaprawajazdy AND
+                                                   input_Stawka in modele.stawkazadzien;
+        IF(cnt = 0) THEN
+            INSERT INTO Modele(model, pojemnoscsilnika, sredniespalanie, kategoriaprawajazdy, stawkazadzien)
+            VALUES(input_Model, input_Pojemnosc, input_Spalanie, input_KatPrawaJazdy, input_Stawka);
+        END IF;
+    END;
+END;
+/
+create or replace procedure insert_new_pojazd(input_NumerVIN in pojazdy.numervin%TYPE,
+                                              input_NumerRej in pojazdy.numerrejestracyjny%TYPE,
+                                              input_Rocznik in pojazdy.rocznik%TYPE,
+                                              input_Przebieg in pojazdy.przebieg%TYPE,
+                                              input_DataPrzegladu in pojazdy.datawaznosciprzegladu%TYPE,
+                                              input_Uszkodzony in pojazdy.uszkodzony%TYPE,
+                                              input_Model in modele.model%TYPE,
+                                              input_Pojemnosc in modele.pojemnoscsilnika%TYPE,
+                                              input_Spalanie in modele.sredniespalanie%TYPE,
+                                              input_KatPrawaJazdy in modele.kategoriaprawajazdy%TYPE,
+                                              input_Stawka in modele.stawkazadzien%TYPE) IS 
+BEGIN
+    DECLARE 
+    index_value INT := 0;
+    cnt INT := 0;
+    BEGIN
+        SELECT COUNT(*) INTO cnt FROM Pojazdy WHERE input_NumerVIN in pojazdy.numervin;
+        IF(cnt = 0) THEN
+            insert_new_model(input_Model, input_Pojemnosc, input_Spalanie, input_KatPrawaJazdy, input_Stawka);
+            SELECT ID_Modelu INTO index_value FROM Modele WHERE input_Model in modele.model AND
+                                                                input_Pojemnosc in modele.pojemnoscsilnika AND
+                                                                input_Spalanie in modele.sredniespalanie AND
+                                                                input_KatPrawaJazdy in modele.kategoriaprawajazdy AND
+                                                                input_Stawka in modele.stawkazadzien;
+
+            INSERT INTO Pojazdy(id_modelu, numervin, numerrejestracyjny, rocznik, przebieg, datawaznosciprzegladu, uszkodzony)
+            VALUES(index_value, input_NumerVIN, input_NumerRej, input_Rocznik, input_Przebieg, input_DataPrzegladu, input_Uszkodzony);
+        END IF;
+    END;
+END;
+/
 COMMIT;
 
